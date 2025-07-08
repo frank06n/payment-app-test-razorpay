ce8c75b06e1c27582b7fc9795fe157a573f44483
frank06n <dev.code38@gmail.com>
2025-07-08 21:20:56 +0530

    dev apk built & test - test api key

--- App.tsx
+++ App.tsx
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+    Button,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+import {
+    Colors,
+    Header,
+} from 'react-native/Libraries/NewAppScreen';
+
+import RazorpayCheckout from 'react-native-razorpay';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const isDarkMode = useColorScheme() === 'dark';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
+
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+            <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+                style={backgroundStyle}>
+                <Header />
+                <View
+                    style={{
+                        backgroundColor: isDarkMode ? Colors.black : Colors.white,
+                    }}>
+                    <Button
+                        title={'Pay with Razorpay'}
+                        onPress={() => {
+                            var options = {
+                                description: 'Credits towards consultation',
+                                image: 'https://cdn-icons-png.flaticon.com/512/1019/1019607.png',
+                                currency: 'INR',
+                                key: 'rzp_test_6pwO2qRP7wivv5', // Your api key
+                                amount: '10',
+                                name: 'Payer name',
+                                prefill: {
+                                    email: 'payer@razorpay.com',
+                                    contact: '9191919191',
+                                    name: 'Razorpay Software',
+                                },
+                                theme: { color: '#F37254' },
+                            };
+                            RazorpayCheckout.open(options)
+                                .then((data: any) => {
+                                    // handle success
+                                    alert(`Success: ${data.razorpay_payment_id}`);
+                                })
+                                .catch((error: any) => {
+                                    // handle failure
+                                    alert(`Error: ${error.code} | ${error.description}`);
+                                });
+                        }}
+                    />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
